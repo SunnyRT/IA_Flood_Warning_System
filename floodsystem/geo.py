c6d72330bb7e388243392dc589a455820c5753cf
Luke Lawton <lrl35@cam.ac.uk>
2022-01-22 22:55:32 +0000

    Completed Task 1B, includes new addition to floodsystem\geo

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,17 +7,18 @@
 geographical data.
 
 """
-from .station import MonitoringStation
-from .stationdata import build_station_list
 from .utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
 
 def stations_by_distance(stations, p):
     """Function to return a sorted list of (station, distance) tuple by distance (kilometres) from coordinate p."""
+    # Create empty list
     stationlist = []
     for i in stations:
+        # Find the distance for each station and couple it up with its name into a list of tuples
         dist = haversine(i.coord , p)
         stationtuple = (i.name, dist)
         stationlist.append(stationtuple)
+    # Sort the stations by distance from p
     sortedstations = sorted_by_key(stationlist, 1, reverse=False)
     return sortedstations
